8194936d27e703c9c388e9fcc1d88393b726b4f9
jeefumba <jeefrader2@gmail.com>
2021-03-31 14:52:48 +0200

    First commit

--- src/test/java/za/ac/cput/RepertoryTest.java
+++ src/test/java/za/ac/cput/RepertoryTest.java
@@ -1,6 +1,9 @@
 package za.ac.cput;
 
+import org.junit.Assert;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.ignore;
+import org.junit.jupiter.api.Test;
 
 
 class RepertoryTest {
