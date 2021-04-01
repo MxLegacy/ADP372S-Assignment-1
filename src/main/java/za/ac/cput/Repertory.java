36ca7bb030e7a9c2dceaf48dec63742e34e6784a
jeefumba <jeefrader2@gmail.com>
2021-04-01 14:25:11 +0200

    Second commit

--- src/main/java/za/ac/cput/Repertory.java
+++ src/main/java/za/ac/cput/Repertory.java
@@ -1,9 +1,53 @@
 package za.ac.cput;
 /*
 * @reppertory.java
-* Author : Joseph Djamba
+* Author : Joseph Djamba 218292694
 * This program demonstrate the test driven developmnt in the application
 * */
 public class Repertory {
+    public String contactName;
+    public String ContactSurname;
+    public int ContactNumber;
+    public String emailAdress;
 
+
+    public Repertory(String contactName, String ContactSurname, int ContactNumber, String emailAdress) {
+        contactName = contactName;
+        ContactSurname = ContactSurname;
+        ContactNumber = ContactNumber;
+        emailAdress = emailAdress;
+
+    }
+
+    public String getContactName() {
+        return contactName;
+    }
+
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    public String getContactSurname() {
+        return ContactSurname;
+    }
+
+    public void setContactSurname(String contactSurname) {
+        ContactSurname = contactSurname;
+    }
+
+    public int getContactNumber() {
+        return ContactNumber;
+    }
+
+    public void setContactNumber(int contactNumber) {
+        ContactNumber = contactNumber;
+    }
+
+    public String getEmailAdress() {
+        return emailAdress;
+    }
+
+    public void setEmailAdress(String emailAdress) {
+        this.emailAdress = emailAdress;
+    }
 }
--- src/test/java/za/ac/cput/RepertoryTest.java
+++ src/test/java/za/ac/cput/RepertoryTest.java
@@ -1,11 +1,24 @@
 package za.ac.cput;
 
-//import org.junit.Assert;
-import static org.junit.jupiter.api.Assertions.*;
-//import org.junit.Ignore;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.*;
 
 class RepertoryTest {
+    private Repertory conctact1;
+    private Repertory conctact2;
+    private Repertory conctact3;
+    private Repertory conctact4;
+    @BeforeEach
+    void setUp() {
+        conctact1 = new Repertory("Joseph","umba",9876, "Jos@gmail.com");
+        conctact2 = new Repertory("plamedie","bitota",1234,"bit@gmail.com");
+        conctact3 = conctact1;
+    }
+    @Test
+    public void testConctactIdentity () {
 
+
+    }
 }
